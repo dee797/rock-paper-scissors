e0348c70006f700c54b221c665605f6518bce01b
dee <178827694+dee797@users.noreply.github.com>
2024-09-01 13:03:50 -0600

    Added getHumanChoice()
    This function prompts the user to enter a number
    for one of the following choices: 1. Rock,
    2. Paper, or 3. Scissors. If the user enters
    something other than an integer, the script
    outputs an alert message to let them know that
    they must enter an integer between 1 and 3. The
    function then returns the value in the array
    `strings` associated with the index userInput-1.

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+function getHumanChoice() {
+    const strings = ["rock", "paper", "scissors"];
+    var userInput = parseInt(prompt("Enter the number for one of the choices below:\n1. Rock\n2. Paper\n3. Scissors"));
+
+    while (isNaN(userInput)) {
+        
+        alert("Invalid entry. Please enter a number from 1 to 3.")
+        var userInput = parseInt(prompt("Enter the number of one of the choices below:\n1. Rock\n2. Paper\n3. Scissors"))
+    }
+    
+    console.log(strings[userInput-1])
+    return strings[userInput-1]
+}
+
 
 function getComputerChoice() {
     const strings = ["rock", "paper", "scissors"];
-    let num = Math.floor(Math.random()* 3);
-    console.log(num);
+    const randomNum = Math.floor(Math.random()* 3);
+    return strings[randomNum]
 }
 
-getComputerChoice();
+//test
+getHumanChoice();
