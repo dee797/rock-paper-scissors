0a2e6077349ab1b36316126426dea88c8c6e789a
dee <178827694+dee797@users.noreply.github.com>
2024-09-01 14:08:20 -0600

    Added playRound(humanChoice, computerChoice)
    This function lets you play a round of Rock Paper
    Scissors with the computer. The constants
    humanChoice and computerChoice call the
    getHumanChoice() and getComputerChoice() functions
    respectively. These constants are then passed as
    arguments to a playRound() function call. Within
    the function, values of humanChoice and
    computerChoice are checked against each other to
    determine a winner or if there was a tie.

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getHumanChoice() {
     const strings = ["rock", "paper", "scissors"];
     var userInput = parseInt(prompt("Enter the number for one of the choices below:\n1. Rock\n2. Paper\n3. Scissors"));
 
-    while (isNaN(userInput)) {
+    while (isNaN(userInput) || userInput > 3 || userInput < 1) {
         
-        alert("Invalid entry. Please enter a number from 1 to 3.")
-        var userInput = parseInt(prompt("Enter the number of one of the choices below:\n1. Rock\n2. Paper\n3. Scissors"))
+        alert("Invalid entry. Please enter a number from 1 to 3.");
+        var userInput = parseInt(prompt("Enter the number for one of the choices below:\n1. Rock\n2. Paper\n3. Scissors"));
     }
-    
-    console.log(strings[userInput-1])
-    return strings[userInput-1]
-}
 
+    return strings[userInput-1];
+}
 
 function getComputerChoice() {
     const strings = ["rock", "paper", "scissors"];
     const randomNum = Math.floor(Math.random()* 3);
-    return strings[randomNum]
+    return strings[randomNum];
 }
 
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === 'rock' && computerChoice === 'paper') ||
+        (humanChoice === 'paper' && computerChoice === 'scissors') ||
+        (humanChoice === 'scissors' && computerChoice === 'rock')) {
+        
+        alert(`You lose! You chose ${humanChoice}, which loses to ${computerChoice}.`);
+        computerScore++
+
+    } else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+               (humanChoice === 'paper' && computerChoice === 'rock') ||
+               (humanChoice === 'scissors' && computerChoice === 'paper')) {
+        
+        alert(`You win! You chose ${humanChoice}, which beats ${computerChoice}.`);
+        humanScore++
+
+    } else {
+
+        alert(`It's a tie! You both chose ${humanChoice}`);
+    }
+}
+
+
 //test
-getHumanChoice();
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
