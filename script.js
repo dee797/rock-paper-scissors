2f3bff16cfeb84ec3fb021993f25f5b938570603
dee <178827694+dee797@users.noreply.github.com>
2024-09-03 15:36:33 -0600

    Changes to index.html and script.js
    In index.html: added an h1 heading, a description
    of the rules of rock paper scissors, and a button
    to start the game. The button's onclick property
    will call the function playGame(). This function
    allows the user to play the game for 5 rounds by
    dynamically calling the playRound() function nested
    within it. The humanScore and computerScore
    variables are incremented each round depending on
    whoever's the winner or not. After the five rounds,
    the final winner of the game is calculated and
    shown to the user.

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-let humanScore = 0;
-let computerScore = 0;
+document.querySelector("button").onclick = playGame;
+
 
 function getHumanChoice() {
     const strings = ["rock", "paper", "scissors"];
-    var userInput = parseInt(prompt("Enter the number for one of the choices below:\n1. Rock\n2. Paper\n3. Scissors"));
+    let userInput = prompt("Enter the number for one of the choices below:\n1. Rock\n2. Paper\n3. Scissors");
+    
 
-    while (isNaN(userInput) || userInput > 3 || userInput < 1) {
+    while (userInput !== null) {
+        const temp = parseInt(userInput);
         
-        alert("Invalid entry. Please enter a number from 1 to 3.");
-        var userInput = parseInt(prompt("Enter the number for one of the choices below:\n1. Rock\n2. Paper\n3. Scissors"));
+        if (isNaN(temp) || temp > 3 || temp < 1) {
+            alert("Invalid entry. Please enter a number from 1 to 3.");
+            userInput = prompt("Enter the number for one of the choices below:\n1. Rock\n2. Paper\n3. Scissors");
+            
+        } else {
+            return strings[temp-1];
+        }
+        
     }
 
-    return strings[userInput-1];
+    if (userInput === null) throw "Stop script"
 }
+
 
 function getComputerChoice() {
     const strings = ["rock", "paper", "scissors"];
@@ -20,30 +29,52 @@ function getComputerChoice() {
     return strings[randomNum];
 }
 
-function playRound(humanChoice, computerChoice) {
-    if ((humanChoice === 'rock' && computerChoice === 'paper') ||
-        (humanChoice === 'paper' && computerChoice === 'scissors') ||
-        (humanChoice === 'scissors' && computerChoice === 'rock')) {
-        
-        alert(`You lose! You chose ${humanChoice}, which loses to ${computerChoice}.`);
-        computerScore++
 
-    } else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
-               (humanChoice === 'paper' && computerChoice === 'rock') ||
-               (humanChoice === 'scissors' && computerChoice === 'paper')) {
-        
-        alert(`You win! You chose ${humanChoice}, which beats ${computerChoice}.`);
-        humanScore++
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
+    function playRound(humanChoice, computerChoice) {
+        if ((humanChoice === 'rock' && computerChoice === 'paper') ||
+            (humanChoice === 'paper' && computerChoice === 'scissors') ||
+            (humanChoice === 'scissors' && computerChoice === 'rock')) {
+            
+            alert(`You lose!\n
+                   You chose ${humanChoice}, and the computer chose ${computerChoice}.
+                   Unfortunately, ${humanChoice} loses to ${computerChoice}.`);
+            computerScore++
+    
+        } else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+                   (humanChoice === 'paper' && computerChoice === 'rock') ||
+                   (humanChoice === 'scissors' && computerChoice === 'paper')) {
+            
+            alert(`You win!\n
+                   You chose ${humanChoice}, and the computer chose ${computerChoice}.
+                   ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} beats ${computerChoice}!`);
+            humanScore++
+    
+        } else {
+    
+            alert(`It's a tie! You both chose ${humanChoice}.`);
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        alert(`This is turn ${i+1} out of 5`)
+
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+
+    if (humanScore > computerScore) {
+        alert(`You won the game!\nYour score was ${humanScore}, and the computer's score was ${computerScore}.\n
+You won by ${humanScore-computerScore} point(s).`)
+    } else if (humanScore === computerScore) {
+        alert(`It's a tie!\nYour score and the computer's score were both ${humanScore}.`)
     } else {
-
-        alert(`It's a tie! You both chose ${humanChoice}`);
+        alert(`You lost the game!\nYour score was ${humanScore}, and the computer's score was ${computerScore}.\n
+You lost by ${computerScore-humanScore} point(s).`)
     }
 }
 
-
-//test
-
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice);
