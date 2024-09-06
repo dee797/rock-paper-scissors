e2c1046bf7c4c52471d0d336e32c5d2e7c966645
dee <178827694+dee797@users.noreply.github.com>
2024-09-06 12:06:40 -0600

    Cleaned up code and logic in script.js
    Debugged an error on line 84 where
    querySelector("#results") was returning null.
    The problem was that the query was not specific
    enough, so the string "body > #results" had to be
    passed to the function.
    
    Also fixed another logic error on line 91 where
    event listeners are added to each choice
    button. No runtime error was raised, but the problem
    was that the playRound() function was being passed
    as an argument to the event listener instead of being
    a callback function.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const CHOICES = ["rock", "paper", "scissors"];
 let btn = document.querySelector("#start")
-btn.addEventListener('click', playGame)
+btn.addEventListener('click', playGame, {once:true})
 
 
 
@@ -26,19 +26,22 @@ function setupGameElements() {
         temp.innerText = `${CHOICES[i].charAt(0).toUpperCase()}${CHOICES[i].substring(1)}`
         document.body.appendChild(temp);
     }
+    
+    const roundResults = document.createElement("h2");
+    roundResults.setAttribute("id", "results");
+    roundResults.textContent = " "
+    document.body.appendChild(roundResults);
 
     const runningScore = document.createElement("p");
     runningScore.setAttribute("id", "runningScore")
     document.body.appendChild(runningScore);
-    
-    const roundResults = document.createElement("p");
-    roundResults.setAttribute("id", "results");
-    document.body.appendChild(roundResults);
 
     const nextBtn = document.createElement("button");
     nextBtn.setAttribute("id", "next");
-    document.body.appendChild(nextBtn);
+    nextBtn.textContent = "Next round";
     nextBtn.style.display = "none";
+    document.body.appendChild(nextBtn);
+    nextBtn.addEventListener("click", () => toggleChoicesDisplay(true));
     
 }
 
@@ -53,6 +56,7 @@ function toggleChoicesDisplay(flag) {
     if (flag) {
         desc.style.display = "block";
         roundResults.style.display = "none";
+        nextBtn.style.display = "none";
 
         for (const choice of choiceButtons) {
             choice.style.display = "inline";
@@ -72,59 +76,68 @@ function toggleChoicesDisplay(flag) {
 
 function playGame() {
     setupGameElements();
-    toggleChoicesDisplay(true);
 
     let humanScore = 0;
     let computerScore = 0;
 
-    const choices = document.querySelectorAll(".choices")
-    for (const choice of choices) {
-        choice.addEventListener("click", playRound(`'${choice.id}'`, getComputerChoice()))
-    }
+    // const roundResults = document.querySelectorAll("#results")[0]; does the same thing
+    const roundResults = document.querySelector("body > #results");
 
-    const roundResults = document.querySelector("#results");
+    const runningScore = document.querySelector("body > #runningScore");
+    runningScore.innerText = `Your score: ${humanScore}\nComputer score: ${computerScore}`;
+
+    const choices = document.querySelectorAll(".choices");
+    for (const choice of choices) {
+        choice.addEventListener("click", () => playRound(`${choice.id}`, getComputerChoice()));
+    }
 
 
 
     function playRound(humanChoice, computerChoice) {
     
-        while (humanScore < 5 || computerScore < 5) {
+        if (humanScore < 5 || computerScore < 5) {
             if ((humanChoice === 'rock' && computerChoice === 'paper') ||
-            (humanChoice === 'paper' && computerChoice === 'scissors') ||
-            (humanChoice === 'scissors' && computerChoice === 'rock')) {
+                (humanChoice === 'paper' && computerChoice === 'scissors') ||
+                (humanChoice === 'scissors' && computerChoice === 'rock')) {
             
-            roundResults.textContent= `You lose this round!\n
-            You chose ${humanChoice}, and the computer chose ${computerChoice}.
-            Unfortunately, ${humanChoice} loses to ${computerChoice}.`;
-            computerScore++
+                roundResults.textContent = `You lose this round!\n
+                You chose ${humanChoice}, and the computer chose ${computerChoice}.
+                Unfortunately, ${humanChoice} loses to ${computerChoice}.`;
+                computerScore++
     
             } else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
-                   (humanChoice === 'paper' && computerChoice === 'rock') ||
-                   (humanChoice === 'scissors' && computerChoice === 'paper')) {
+                (humanChoice === 'paper' && computerChoice === 'rock') ||
+                (humanChoice === 'scissors' && computerChoice === 'paper')) {
             
-            roundResults.textContent =`You win this round!\n
-            You chose ${humanChoice}, and the computer chose ${computerChoice}.
-            ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} beats ${computerChoice}!`;
-            humanScore++
+                roundResults.textContent = `You win this round!\n
+                You chose ${humanChoice}, and the computer chose ${computerChoice}.
+                ${humanChoice[0].toUpperCase()}${humanChoice.substring(1)} beats ${computerChoice}!`;
+                humanScore++
     
             } else {
     
-            roundResults.textContent = `It's a tie! You both chose ${humanChoice}.`;
+                roundResults.textContent = `It's a tie! You both chose ${computerChoice}.`;
             }
-    
-    
-            toggleChoicesDisplay(false);
+        }
+
+        
+        runningScore.innerText = `Your score: ${humanScore}\nComputer score: ${computerScore}`;
+        toggleChoicesDisplay(false);
+
+
+        if (humanScore === 5) {
+            
+            roundResults.textContent = `You won the game!\nYou reached 5 points first.\n
+            You won by ${humanScore-computerScore} point(s).`;
+            document.body.removeChild(document.querySelector("body > #next"));
+
+        } else if (computerScore === 5) {
+
+            roundResults.textContent = `You lost the game!\nThe computer reached 5 points first.\n
+            You lost by ${computerScore-humanScore} point(s).`
+            document.body.removeChild(document.querySelector("body > #next"));
+            
         }
     }
-
-
-    if (humanScore === 5) {
-        roundResults.textContent = `You won the game!\nYour score was ${humanScore}, and the computer's score was ${computerScore}.\n
-You won by ${humanScore-computerScore} point(s).`;
-    } else if (humanScore === computerScore) {
-        roundResults.textContent = `It's a tie!\nYour score and the computer's score were both ${humanScore}.`;
-    } else {
-        roundResults.textContent = `You lost the game!\nYour score was ${humanScore}, and the computer's score was ${computerScore}.\n
-You lost by ${computerScore-humanScore} point(s).`
-    }
+    
 }
